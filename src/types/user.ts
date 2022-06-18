c5bb1cbf3d56df65271ca4807b553b3e90078967
kekoawong <kwong6@nd.edu>
2022-06-17 22:05:30 -0700

    Changed user type

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,7 +1,7 @@
 export interface User {
     uid: string | undefined,
-    email: string | undefined,
-    phoneNumber: string | undefined,
+    email: string | undefined | null,
+    phoneNumber: string | undefined | null,
     isAnonymous: boolean | undefined,
     emailVerified: boolean | undefined,
     loggedIn: boolean | undefined
