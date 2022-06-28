def9a92a7720ecd6ac49d72c08e9a452083bb80a
kekoawong <kwong6@nd.edu>
2022-06-27 17:32:34 -0700

    Added eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,5 +31,13 @@ module.exports = {
         "react"
     ],
     "rules": {
+        // using arrow function components: https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/function-component-definition.md#rule-options
+        "react/function-component-definition": [2, { "namedComponents": "arrow-function" }],
+        // props spreading allowed for jsx components: https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-props-no-spreading.md#rule-details
+        "react/jsx-props-no-spreading": [2, {"custom": "ignore"}],
+        // default exports not preferred
+        "import/prefer-default-export": "off",
+        // for managing state with redux toolkit: https://redux-starter-kit.js.org/usage/immer-reducers#linting-state-mutations
+        "no-param-reassign": ["error", { props: true, ignorePropertyModificationsFor: ["state"] }],
     }
 };
