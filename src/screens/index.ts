a14e74116ad209a42cc68e18e8427c1d4a446490
kekoawong <kwong6@nd.edu>
2022-08-11 11:38:38 -0400

    Changed all screen names due to github error

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,6 +1,6 @@
-import { HomeScreen } from 'src/screens/home';
-import { ExploreScreen } from 'src/screens/explore';
-import { WelcomeScreen } from 'src/screens/welcome';
-import { LoginScreen } from 'src/screens/login';
+import { HomeScreen } from 'src/screens/home-screen';
+import { ExploreScreen } from 'src/screens/explore-screen';
+import { WelcomeScreen } from 'src/screens/welcome-screen';
+import { LoginScreen } from 'src/screens/login-screen';
 
 export { HomeScreen, ExploreScreen, WelcomeScreen, LoginScreen };
