eb4b238442ebff8393048a77172806245f1ee2a4
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-11-06 13:48:24 -0500

    added storybook and able to view page on expo

--- App.ts
+++ App.ts
@@ -1,5 +1,11 @@
-import 'react-native-gesture-handler';
+// import 'react-native-gesture-handler';
 
-import { App } from './src';
+// import { App } from './src';
 
-export default App;
+// export default App;
+
+// App.tsx
+const STORYBOOK_START = true;
+export default STORYBOOK_START
+? require('./storybook').default
+: require('./src').default;
