a30d606349d68b3a1c82a0c826cd78f2d8cbd4f7
kekoawong <kwong6@nd.edu>
2022-11-16 11:36:17 -0500

    Configured eslint configuration

---
+++ functions/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    bracketSpacing: true,
+    bracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 100,
+    semi: true,
+    tabWidth: 4
+  };
+  
