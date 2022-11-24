13c87f7f218a59e1dd2611d9e774a71fdf021fb9
kekoawong <kwong6@nd.edu>
2022-11-24 10:06:49 -0500

    Deleted unused wrappers

--- src/components/wrappers/index.ts
+++
@@ -1,4 +0,0 @@
-import { KeyboardBehaviorWrapper, KeyboardBehaviorProps } from './keyboard-behavior';
-import { BottomModal, BottomModalProps } from './bottom-modal';
-
-export { KeyboardBehaviorWrapper, KeyboardBehaviorProps, BottomModal, BottomModalProps };
