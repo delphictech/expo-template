45201ea051f8996834b7488a5510689ad019fd2f
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-11-28 11:24:06 -0500

    used converted to fix type coming back from firebase

--- src/firebase/image-api.ts
+++ src/firebase/image-api.ts
@@ -2,28 +2,31 @@ import { storage, db } from 'src/firebase/config';
 import { getDownloadURL, ref, uploadBytesResumable, uploadBytes } from 'firebase/storage';
 import { upLoadFile } from 'src/utils/upload-image';
 import { getDoc, doc } from 'firebase/firestore';
+import { publicUserCollection } from 'src/firebase/config';
 
 export const fetchUserImage = async (
     userID: string,
     imgURI: string | undefined,
-): Promise<string> => {
+): Promise<string | null> => {
     console.log('this is from the fetch user imag function');
     const storageRef = ref(storage, `user-profile-img/${userID}`);
 
     // let image: string | Promise<string>;
     if (!imgURI) {
-        const docRef = doc(db, 'public-user-data', userID);
+        // const docRef = doc(db, 'public-user-data', userID);
+        const docRef = doc(publicUserCollection, userID);
+
         const docSnap = await getDoc(docRef);
-        const data = docSnap.data() as string;
-        // const image = await getDownloadURL(storageRef).then((url) => {
-        //     return url;
-        // });
-        // console.log('image from undefined', image);
-        // return image;
-        // console.log('doc snapshot', docSnap);
-        return data.image;
+        const data = docSnap.data();
+
+        if (data?.image) {
+            return data?.image;
+        } else {
+            return null;
+        }
+    } else {
+        console.log('upload function firing');
+        await upLoadFile(imgURI, userID);
+        return imgURI;
     }
-    console.log('upload function firing');
-    await upLoadFile(imgURI, userID);
-    return imgURI;
 };
